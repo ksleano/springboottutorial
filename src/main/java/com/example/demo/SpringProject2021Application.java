25364ec199b6b7e7602ea4ab202359cab4686a8f
ksl <ksleano@gmail.com>
2021-07-23 10:08:15 -0700

    init

---
+++ src/main/java/com/example/demo/SpringProject2021Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProject2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProject2021Application.class, args);
+	}
+
+}
